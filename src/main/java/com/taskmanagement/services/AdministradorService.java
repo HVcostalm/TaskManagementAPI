78255597b6ef94b058d60faf6085647b3730d6e8
Heitor Vinicius <96549289+HVcostalm@users.noreply.github.com>
2024-10-02 17:00:51 -0300

    Delete src/main/java/com/taskmanagement/services directory

--- src/main/java/com/taskmanagement/services/AdministradorService.java
+++
@@ -1,8 +0,0 @@
-package com.taskmanagement.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AdministradorService {
-	// Esperar Quarta Para Ver Sobre Isso
-}
--- src/main/java/com/taskmanagement/services/FuncionarioSeniorService.java
+++
@@ -1,8 +0,0 @@
-package com.taskmanagement.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FuncionarioSeniorService {
-	// Esperar Quarta Para Ver Sobre Isso
-}
